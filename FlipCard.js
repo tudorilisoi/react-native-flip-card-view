f94ac07a9c586158b8f6554bfeda5e4dc1eea01f
Tudor Ilisoi <tudorilisoi@yahoo.com>
2018-04-04 17:39:54 +0300

    avoid re-render

--- FlipCard.js
+++ FlipCard.js
@@ -1,4 +1,4 @@
-import React,{Component} from "react";
+import React from "react";
 import {
     Animated,
     Easing,
@@ -7,63 +7,98 @@ import {
     View
 } from "react-native";
 
+import {screen} from 'src/style'
+
 const styles = {
+    animatedFace: {
+        opacity: .5,
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: screen.width,
+        height: screen.height - screen.navh
+    },
     animatedContainer: {
-        flex: 1
+        // flex: 1,
+        top: 0,
+        left: 0,
+        width: screen.width,
+        height: screen.height - screen.navh
     }
 };
 
-class FlipCard extends Component{
-    constructor(){
+class FlipCard extends React.PureComponent {
+    constructor() {
         super();
-        this._flipToggleCard = this._flipToggleCard.bind(this);
-        this._flippedCard = this._flippedCard.bind(this);
-        this.flippedCardView = this.flippedCardView.bind(this);
+
         this.state = {
             animatedValue: new Animated.Value(0),
+            opacityFront: new Animated.Value(1),
+            opacityBack: new Animated.Value(0),
             isFlipped: true,
-        };
-    };
-
-    componentDidUpdate(prevProp, prevState) {
-        if (this.state.isFlipped !== prevState.isFlipped) {
-            this._flippedCard();
         }
-    };
 
-    _flipToggleCard() {
-        this.setState({isFlipped: !this.state.isFlipped});
-    };
+        this.state.animatedValue.addListener((valueObj) => {
+            const flipped = valueObj.value >= 0.5
+            this.state.opacityBack.setValue(flipped ? 1 : 0)
+            this.state.opacityFront.setValue(flipped ? 0 : 1)
+            console.log('ANIM', valueObj.value, flipped);
+        });
 
-    _flippedCard() {
+    }
+
+    _flipToggleCard = () => {
+        // this.setState({isFlipped: !this.state.isFlipped});
+        this.flipped = !this.flipped
         Animated.spring(this.state.animatedValue, {
-            toValue: 0,   // Returns to the start
+            toValue: this.flipped ? 1 : 0,   // Returns to the start
             velocity: this.props.velocity,  // Velocity makes it move
             tension: this.props.tension, // Slow
             friction: this.props.friction,  // Oscillate a lot
-        }).start();
+        }).start()
     };
 
     render() {
-        const rotateX = this.state.animatedValue.interpolate({
+        const rotateYFront = this.state.animatedValue.interpolate({
             inputRange: [0, 0.5, 1],
-            outputRange: ['0deg', '360deg', '0deg']
+            outputRange: ['0deg', '-90deg', '180deg']
         });
+
+        const rotateYBack = this.state.animatedValue.interpolate({
+            inputRange: [0, 0.5, 1],
+            outputRange: ['180deg', '-90deg', '0deg']
+        });
+
+        const frontFaceStyle = [
+            styles.animatedFace,
+            {opacity: this.state.opacityFront, transform: [{rotateY: rotateYFront}]}
+        ]
+        const backFaceStyle = [
+            styles.animatedFace,
+            {opacity: this.state.opacityBack, transform: [{rotateY: rotateYBack}]}
+        ]
+
         return (
-            <TouchableOpacity onPress={this._flipToggleCard} style={styles.animatedContainer}>
+            <TouchableOpacity onPress={this._flipToggleCard} style={[styles.animatedContainer, {zIndex: -1}]}>
                 <Animated.View
-                    style={[styles.animatedContainer,{transform: [{rotateX}]}]}>
-                    {this.flippedCardView(this.state.isFlipped)}
+                    useNativeDriver
+                    style={frontFaceStyle}>
+                    {this.flippedCardView(false)}
                 </Animated.View>
-            </TouchableOpacity>);
-    };
+                <Animated.View
+                    useNativeDriver
+                    style={backFaceStyle}>
+                    {this.flippedCardView(true)}
+                </Animated.View>
+            </TouchableOpacity>)
+    }
 
 
-    flippedCardView(isFlipped) {
-        if (isFlipped) {
-            return this.props.renderFront;
+    flippedCardView = (isFlipped) => {
+        if (!isFlipped) {
+            return this.props.renderFront
         } else {
-            return this.props.renderBack;
+            return this.props.renderBack
         }
     }
 }
