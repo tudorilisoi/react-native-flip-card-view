3315992dba4434b4e7a132fc5cfca89129edfde2
Tudor Ilisoi <tudorilisoi@yahoo.com>
2019-04-24 14:59:29 +0300

    FLipCard

--- FlipCard.js
+++ FlipCard.js
@@ -1,7 +1,14 @@
+// @see https://github.com/yasemincidem/Flip-Card
+
 import React from "react";
-import {Animated, View, Easing} from "react-native";
+import {Animated, View, Easing, InteractionManager} from "react-native";
 import PropTypes from 'prop-types';
 import {screen} from 'src/style'
+import _ from 'lodash';
+import connectstore from "src/lib/thinflux/src/connectstore"
+import store from 'src/store'
+import Loader from 'src/unicenta/components/Loader'
+
 
 const computeStyles = (containerSize) => {
     return {
@@ -27,12 +34,19 @@ class FlipCard extends React.PureComponent {
     static propTypes = {
         containerSize: PropTypes.object.isRequired, //{width,height}
         style: PropTypes.any,
-        flipped: PropTypes.bool
+        flipped: PropTypes.bool,
+        renderFront: PropTypes.any.isRequired,
+        renderBack: PropTypes.any.isRequired,
+        onStart: PropTypes.func,
+        onEnd: PropTypes.func,
     }
 
-    static defaultProps = {
+    static
+    defaultProps = {
         style: null,
         initiallyFlipped: false,
+        onStart: _.noop,
+        onEnd: _.noop,
     }
 
     constructor(props) {
@@ -42,11 +56,12 @@ class FlipCard extends React.PureComponent {
         this.state = {
             animatedValue: new Animated.Value(initial),
             isFlipped: new Animated.Value(initial),
+            animating: false,
         }
 
         //using negative left position to prevent touches on the hidden face
 
-        this.state.frontPosition = Animated.multiply(this.state.isFlipped, -300 * screen.vmax)
+        this.state.frontPosition = Animated.multiply(this.state.isFlipped, -300 * screen.width)
         this.state.backPosition = Animated.multiply(Animated.add(this.state.isFlipped, -1), 300 * screen.vmax)
 
 
@@ -83,16 +98,20 @@ class FlipCard extends React.PureComponent {
 
         this.flipped = !this.flipped
 
+        const {onStart, onEnd} = this.props
+        // onStart()
         Animated.timing(this.state.animatedValue, {
             useNativeDriver: true,
-            duration: 250,
+            duration: 300,
             easing: Easing.inOut(Easing.sin),
             toValue: this.flipped ? 1 : 0
         }).start(() => {
-            // console.log('ANIMATION DONE');
+            console.log('ANIMATION DONE');
+            // onEnd()
             return true
         })
-    };
+    }
+
 
     render() {
         console.log('RENDER FLIP');
@@ -136,9 +155,11 @@ class FlipCard extends React.PureComponent {
             //if memory problems/GPU problems, enable before animating and disable afterwards
 
             <View
+                key={'flip-wrapper'}
                 style={[styles.animatedContainer, this.props.style]}>
 
                 <Animated.View
+                    key={'back'}
                     renderToHardwareTextureAndroid={true}
                     shouldRasterizeIOS={true}
                     useNativeDriver
@@ -147,6 +168,7 @@ class FlipCard extends React.PureComponent {
                 </Animated.View>
 
                 <Animated.View
+                    key={'front'}
                     renderToHardwareTextureAndroid={true}
                     shouldRasterizeIOS={true}
                     useNativeDriver
@@ -163,9 +185,20 @@ class FlipCard extends React.PureComponent {
         if (!isFlipped) {
             return this.props.renderFront
         } else {
-            return this.props.renderBack
+            return this.state.animating && false ? (<Loader/>) :
+                this.props.renderBack
         }
     }
 }
 
-module.exports = FlipCard;
+// export default FlipCard;
+
+const {restaurant} = store.actions
+export default connectstore(store, FlipCard, {
+    flipMode: store.actions.ui.FLIP_MODE_PATH,
+    // currentOrder: restaurant.CURRENT_ORDER_PATH,
+    // tableID: restaurant.CURRENT_TABLE_PATH,
+}, function map(values) {
+    console.log('VALUES', values);
+    return {flipped: values.flipMode === store.actions.ui.FLIP_MODE_TICKET}
+})
